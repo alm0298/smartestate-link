a235b5db79c14429a0413802016413ccd96717a6
Ariel Almos <ariel.almos@gmail.com>
2025-03-03 10:34:31 +0200

    Remove built assets from repository

--- assets/browser-C8sj2BZ9.js
+++
@@ -1,2 +0,0 @@
-import{i as e}from"./vendor-ChhLT4kb.js";const o=e((function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")})),t=Object.freeze(Object.defineProperty({__proto__:null,default:o},Symbol.toStringTag,{value:"Module"}));export{t as b};
-//# sourceMappingURL=browser-C8sj2BZ9.js.map
